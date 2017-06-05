d655e5af815ff2689ac01b99b073df931d826377
Alexander Nilov <iraelaxis@gmail.com>
2017-06-05 14:40:58 +0600

    new instruction to nvidia docker

--- scripts/install-nvidia-docker.sh
+++ scripts/install-nvidia-docker.sh
@@ -5,5 +5,11 @@ if which nvidia-docker; then
     exit 0
 fi
 
-wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.1/nvidia-docker_1.0.1-1_amd64.deb
-sudo dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
+wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.1/nvidia-docker_1.0.1_amd64.tar.xz
+sudo tar --strip-components=1 -C /usr/bin -xvf /tmp/nvidia-docker*.tar.xz && rm /tmp/nvidia-docker*.tar.xz
+
+# Run nvidia-docker-plugin
+sudo -b nohup nvidia-docker-plugin > /tmp/nvidia-docker.log
+
+sudo systemctl start nvidia-docker
+sudo systemctl enable nvidia-docker
