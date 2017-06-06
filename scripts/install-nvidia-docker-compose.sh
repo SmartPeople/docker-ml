8c476d9f444701cdda3a74cf854948f23f9953d3
Alexander Nilov <iraelaxis@gmail.com>
2017-06-06 14:42:01 +0600

    volume check in docker-compose

--- scripts/install-nvidia-docker-compose.sh
+++ scripts/install-nvidia-docker-compose.sh
@@ -5,4 +5,9 @@ if which nvidia-docker-compose; then
     exit 0
 fi
 
+if ! docker volume ls | awk '{print $1}' | grep 'nvidia-docker'; then
+    echo "Creating nvidia driver volume..."
+	sudo nvidia-docker run --rm nvidia/cuda nvidia-smi
+fi
+
 pip install nvidia-docker-compose
