42f47454be4e25e1c2fac25a0efb635fa1d1440d
Alexander Nilov <iraelaxis@gmail.com>
2017-06-13 17:19:04 +0600

    New base image, python2 version

--- images/tensorbox/code/start_all.sh
+++ images/tensorbox/code/start_all.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-tensorboard --logdir output \
-& python -u train.py --hypes hypes/overfeat_rezoom.json --gpu 0 --logdir output
+#tensorboard --logdir output \
+#& python -u train.py --hypes hypes/overfeat_rezoom.json --gpu 0 --logdir output & \
+jupyter notebook --ip=0.0.0.0 --allow-root --port=8888 --no-browser
