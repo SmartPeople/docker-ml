4e67fc03c618c5313fbbfdff6d8b5a5daec47a37
Alexander Nilov <iraelaxis@gmail.com>
2017-06-13 17:36:55 +0600

    matplotlib fix

--- images/tensorbox/code/start_all.sh
+++ images/tensorbox/code/start_all.sh
@@ -2,4 +2,4 @@
 
 #tensorboard --logdir output \
 #& python -u train.py --hypes hypes/overfeat_rezoom.json --gpu 0 --logdir output & \
-jupyter notebook --ip=0.0.0.0 --allow-root --port=8888 --no-browser
+jupyter notebook --ip=0.0.0.0 --allow-root --port=8888 --no-browser --NotebookApp.iopub_data_rate_limit=536870912
